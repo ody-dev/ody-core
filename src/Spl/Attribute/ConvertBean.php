efffe2bd8070995b4f44ffc9a0b0afd71ecc9be6
ilyas <deckersilyas@gmail.com>
2025-03-04 23:31:01 +0100

    added Spl classes

---
+++ src/Spl/Attribute/ConvertBean.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ody\Core\Spl\Attribute;
+
+
+use Ody\Core\Spl\SplBean;
+
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
+class ConvertBean
+{
+    private bool $allowNull = false;
+
+
+    function __construct(
+        public string $className
+    ){
+        $ref = new \ReflectionClass($this->className);
+        if(!$ref->isSubclassOf(SplBean::class)){
+            throw new \Exception("{$this->className} not subclass of ".SplBean::class);
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllowNull(): bool
+    {
+        return $this->allowNull;
+    }
+
+    /**
+     * @param bool $allowNull
+     */
+    public function setAllowNull(bool $allowNull): void
+    {
+        $this->allowNull = $allowNull;
+    }
+
+}
